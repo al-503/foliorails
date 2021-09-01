96c0efbd122e7f45118485b3753ea2e1f37fdd72
Axel Ridray <axel.ridray@sfr.fr>
2021-09-01 16:12:02 +0200

    seed and project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Project.destroy_all
+   
+col = Project.create!(name:"Col", description:"Progressive Web App pour aider les utilisateurs pour trouver une colocation.")
+col.save!
+
+retro_world = Project.create!(name:"Retro_World", description:"Maquette Web type Air BnB sur la location de console.")
+retro_world.save!
