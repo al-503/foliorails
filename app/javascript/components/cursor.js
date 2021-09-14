9977bcbbd11384965bd4f3176c20470f4eec1a56
Axel Ridray <axel.ridray@sfr.fr>
2021-09-14 21:25:07 +0200

    cursor test

--- app/javascript/components/cursor.js
+++ app/javascript/components/cursor.js
@@ -4,4 +4,12 @@ document.addEventListener('mousemove', e => {
   cursor.setAttribute('style','top:'+(e.pageY - 20)+ "px; left:"+(e.pageX-20)+"px;")
 })
 
+document.addEventListener('mouseover', () => {
+  cursor.classList.add('expand');
+
+    setTimeout(() => {
+      cursor.classList.remove("expand");
+    }, 500)
+})
+
 export{cursor}
